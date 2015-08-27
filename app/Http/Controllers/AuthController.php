ed4cdfdd4eca2973fe2fc92a525165ed426cb9cf
Jason Raimondi <jason@raimondi.us>
2015-08-26 23:14:24 -0400

    rename auth controller and set middleware jet (although it is blocking the wrong methods now)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,14 +10,11 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
-class AuthenticateController extends Controller
+class AuthController extends Controller
 {
 
     public function __construct()
    {
-       // Apply the jwt.auth middleware to all methods in this controller
-       // except for the authenticate method. We don't want to prevent
-       // the user from retrieving their token if they don't already have it
        $this->middleware('jwt.auth', ['except' => ['authenticate']]);
    }
 
